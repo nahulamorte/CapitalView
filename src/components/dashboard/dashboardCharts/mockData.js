5285930ad5f84dc07821ca88d928b1d2c6cd933f
nahulamorte <nahuellamorte@gmail.com>
2026-03-30 19:02:42 -0300

    creo mock de datos para utilizar en los graficos

---
+++ src/components/dashboard/dashboardCharts/mockData.js
@@ -0,0 +1,9 @@
+export const balanceData = [
+  { name: 'Ene', total: 4000 },
+  { name: 'Feb', total: 3000 },
+  { name: 'Mar', total: 5000 },
+  { name: 'Abr', total: 4500 },
+  { name: 'May', total: 6000 },
+  { name: 'Jun', total: 5500 },
+  { name: 'Jul', total: 8000 },
+];
