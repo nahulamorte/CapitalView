e700a20665fb16ffceab83b1c1328cc62bebdc18
nahulamorte <nahuellamorte@gmail.com>
2026-03-30 19:09:32 -0300

    Creo estructura base

---
+++ src/components/dashboard/dashboardCharts/ChartsContainer.jsx
@@ -0,0 +1,12 @@
+import './ChartsContainer.css'; //css
+
+import { BalanceChart } from './balanceChart/BalanceChart.jsx';
+import { PortfolioPieChart } from './PortfolioPieChart/PortfolioPieChart';
+
+export function ChartsContainer(){
+    return(
+        <div className="chart-container">
+            
+        </div>
+    );
+}
--- src/components/dashboard/dashboardCharts/mockData.js
+++ src/components/dashboard/dashboardCharts/mockData.js
@@ -1,9 +1,16 @@
-export const balanceData = [
-  { name: 'Ene', total: 4000 },
-  { name: 'Feb', total: 3000 },
-  { name: 'Mar', total: 5000 },
-  { name: 'Abr', total: 4500 },
-  { name: 'May', total: 6000 },
-  { name: 'Jun', total: 5500 },
-  { name: 'Jul', total: 8000 },
+
+export const monthlyEvolutionData = [
+    { month: 'Ene', balance: 45000, investment: 40000 },
+    { month: 'Feb', balance: 52000, investment: 45000 },
+    { month: 'Mar', balance: 48000, investment: 45000 }, 
+    { month: 'Abr', balance: 61000, investment: 50000 },
+    { month: 'May', balance: 59000, investment: 50000 },
+    { month: 'Jun', balance: 72000, investment: 55000 },
+];
+
+export const portfolioDistribution = [
+    { name: 'Acciones (Stocks)', value: 45, color: 'var(--primary)' },
+    { name: 'Cripto', value: 25, color: 'var(--primary-light)' },
+    { name: 'Bonos', value: 20, color: 'var(--text-ghost)' },
+    { name: 'Efectivo', value: 10, color: 'var(--border-light)' },
 ];
